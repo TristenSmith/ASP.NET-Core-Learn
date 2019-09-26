cfdcdcfb56354f0421a322ebd239b924a508eae5
Tristen Smith <tristen.smith500@gmail.com>
2019-09-26 09:38:27 -0600

    Coded HelloWorldController

--- MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,9 +9,30 @@ namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        public IActionResult Index()
+        //
+        //GET: /HelloWorld/
+        public string Index()
         {
-            return View();
+            return "This is my default action...";
+        }
+
+        //
+        //GET: /HelloWorld/Welcome/
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method...";
+        //}
+
+        // GET: /HelloWorld/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
